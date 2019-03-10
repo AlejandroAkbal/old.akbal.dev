099f89a3230101179fefe86ed0efcc0c70298850
Alejandro Romero <37181533+VoidlessSeven7@users.noreply.github.com>
2019-03-10 11:48:01 +0000

    Update meter_color.php

--- php/meter_color.php
+++ php/meter_color.php
@@ -96,7 +96,7 @@ echo "<br> <br>";
 
         function wait() {
             setTimeout(function() {
-                window.location = "https://www.akbal.tk/temas"
+                window.location = "https://old.akbal.tk/temas"
             }, 5000);
         }
         wait();
