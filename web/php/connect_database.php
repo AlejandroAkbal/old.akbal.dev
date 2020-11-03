2738a8bbadefcb0cf383f18dee2256d4686e333f
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2020-11-03 13:06:10 +0100

    feat: add database connection script

---
+++ web/php/connect_database.php
@@ -0,0 +1,20 @@
+<?php
+
+$url = getenv('DATABASE_URL');
+$components = parse_url($url);
+
+if ($components) {
+    $hostname = $components['host'];
+    $username = $components['user'];
+    $password = $components['pass'];
+    $database = substr($components['path'], 1);
+    $port = $components['port'];
+}
+
+$dbconnect = mysqli_connect($hostname, $username, $password, $database);
+
+if ($dbconnect->connect_error) {
+    die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+return $dbconnect;
